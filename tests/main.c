58529a9291d5c0878eeac0ffec9503088fe8af2e
João Vitor Espig <jotinha1300@gmail.com>
2023-08-04 19:42:28 -0300

    changed tests

--- tests/main.c
+++ tests/main.c
@@ -8,12 +8,12 @@ int main()
     cg_plot_t *plot = cg_new_plot("Plot", 600, 600);
     for (float x = -90; x <= 105; x += 0.1)
     {
-        float y = x * x + 6 * x + 9; // x² +6x + 9
+        float y = (x + 7) * (x - 10); // x² +6x + 9
         cg_pair_t pair = {x, y};
         cg_plot_add_pair(plot, pair);
     }
     plot->line_thickness = 2;
-    cg_plot_show(plot, -30, 30);
+    cg_plot_show(plot, -10, 13);
     cg_plot_destroy(plot);
     return 0;
 }
